21d54c62ae13e219005b79163ba43180e8dd7fb0
Kimmo Tapaninen <kimmo.tapaninen@devikone.com>
2026-02-24 21:09:44 +0200

    Add Sentry

--- app/error.tsx
+++ app/error.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect } from 'react'
+import * as Sentry from '@sentry/nextjs'
 import { Button } from '@/components/ui/button'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 
@@ -12,6 +13,8 @@ export default function ErrorPage({
   reset: () => void
 }) {
   useEffect(() => {
+    // Send error to Sentry
+    Sentry.captureException(error)
     // Log error to console in development
     console.error('Application error:', error)
   }, [error])
